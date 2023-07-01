04821dbe8fdf8d3914884bbc01c70bea4fa67e9a
DeepaliShar <212deepali.sharma@gmail.com>
2023-07-01 16:03:27 +0530

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+// JavaScript code goes here
+
+// Example JavaScript code
+document.addEventListener('DOMContentLoaded', function() {
+    // Code to be executed after the document has loaded
+  
+    // Example code to handle a click event
+    const button = document.querySelector('.cta-button');
+    button.addEventListener('click', function() {
+      alert('Button clicked!');
+    });
+  });
+  
